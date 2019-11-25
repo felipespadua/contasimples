ea1fca1105d67e85fdf1a7f5cb8cd5f2083506c0
Felipe Padua <felipepadua@MacBook-Pro-de-Felipe.local>
2019-11-25 20:08:44 -0300

    inicio da implementacao, criacao de componentes estaticos de autenticacao, navbar e servicos de autenticacao

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './Components/navbar/Navbar.js';
+import Signup from './components/auth/Signup';
+import AuthService from './services/auth/auth-service';
+import Login from './components/auth/Login';
+import ProtectedRoute from './components/auth/protected-routes';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedInUser: null
+    }
+    this.service = new AuthService();
+    // this.fetchUser = this.fetchUser.bind(this);
+  }
+
+  fetchUser() {
+    if (this.state.loggedInUser === null) {
+      this.service.loggedin()
+        .then(response => {
+          this.setState({
+            loggedInUser: response
+          })
+        })
+        .catch(err => {
+          this.setState({
+            loggedInUser: false
+          })
+        })
+    }
+  }
+
+  getTheUser = (userObj) => {
+    this.setState({
+      loggedInUser: userObj
+    })
+  }
+
+  render() {
+    this.fetchUser();
+    if (this.state.loggedInUser) {
+      return (
+        <div className="App">
+          <Navbar userInSession={this.state.loggedInUser} getUser={this.getTheUser} />
+          <Switch>
+            <ProtectedRoute user={this.state.loggedInUser} exact path='/home' component={Home} />
+          </Switch>
+        </div>
+      );
+    } else {
+      return (
+        <div className="App">
+          <Navbar userInSession={this.state.loggedInUser} />
+          <Switch>
+            <Route exact path='/' render={() => <Login getUser={this.getTheUser} />} />
+            <Route exact path='/signup' render={() => <Signup getUser={this.getTheUser} />} />
+            <ProtectedRoute user={this.state.loggedInUser} exact path='/home' component={Home} />
+          </Switch>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
